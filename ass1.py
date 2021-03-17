04ab69615d11b9281842442e09bb126c8941c53f
amal antony <79002469+amal7654@users.noreply.github.com>
2021-03-17 13:30:03 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,15 @@
+class vehicle:
+    def __init__(self):
+    	print("honda cbr650rr")
+
+    def max_speed(self):
+    	speed=200
+    	print("\n Max_speed:",speed)
+
+    def milage(self):
+        mil=23
+        print("\n Milage:",mil)
+
+s=vehicle()
+s.max_speed()
+s.milage()
